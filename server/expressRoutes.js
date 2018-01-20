8212f398a749156ead98e1b41c66319664c8f11b
Arun K <arunreturns@gmail.com>
2018-01-20 12:08:46 +0000

    Fix mock request

--- server/expressRoutes.js
+++ server/expressRoutes.js
@@ -6,6 +6,40 @@ const fs = require('fs');
 
 module.exports = function(App) {
     App.get("/list", function(req, res){
+        const db = new sqlite3.Database(DBPath);
+        db.serialize(function() {
+            db.all(`SELECT Main.*, DATETIME(last_updated, 'unixepoch') AS last_upd FROM ticker_info Main
+                    INNER JOIN
+                    (SELECT name, MAX(last_updated) AS max_last_updated
+                    FROM ticker_info
+                    GROUP BY name) Grouped 
+                    ON Main.name = Grouped.name 
+                    AND Main.last_updated = Grouped.max_last_updated`, 
+            function(err, rows) {
+                if (err){
+                    console.log(err);
+                    res.status(500).send(err);
+                }
+                res.status(200).send(rows);
+            });
+        });
+        db.close();
+    });
+    
+    App.get('/info/:currencyName', function(req, res){
+        const db = new sqlite3.Database(DBPath);
+        db.all("SELECT *, DATETIME(last_updated, 'unixepoch') AS last_upd FROM ticker_info WHERE name = ? ORDER BY last_updated", req.params.currencyName, function(err, rows) {
+            if (err){
+                console.log(err);
+                res.status(500).send(err);
+            }
+            res.status(200).send(rows);
+        });
+        
+        db.close();
+    });
+    
+    App.get("/mock/list", function(req, res){
         let MockFile = Path.join(__dirname, 'mock', `list.json`);
         if ( fs.existsSync(MockFile)){
             console.log("Mocking..");
@@ -33,7 +67,7 @@ module.exports = function(App) {
         }
     });
     
-    App.get('/info/:currencyName', function(req, res){
+    App.get('/mock/info/:currencyName', function(req, res){
         let MockFile = Path.join(__dirname, 'mock', `${req.params.currencyName}.json`);
         if ( fs.existsSync(MockFile)){
             console.log("Mocking..");
